461019bb15771042a280f5dc9cab859c65f7cfd8
Yuri Doin <yuridoin@gmail.com>
2025-04-25 03:47:33 +0700

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://api:3001/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://142.93.0.157:3001/api';
 
 const api = axios.create({
   baseURL: API_URL,
